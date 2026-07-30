8b756124d4c312e27fa94f806e13a90d5a8af5ac
Skzbots <130946713+saurabh8753@users.noreply.github.com>
2026-07-30 08:41:27 +0530

    Delete app directory

--- app/models/file.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class FileItem(BaseModel):
-    fs_id: str
-    filename: str
-    size: int
-    thumbnail: str | None = None
--- app/routes/subtitle.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import RedirectResponse
-
-router = APIRouter()
-
-@router.get("/subtitle")
-async def subtitle(url: str):
-
-    return RedirectResponse(url)
